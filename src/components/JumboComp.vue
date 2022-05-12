d13d8dde0c85f07ee2bcda33c54ba89a940927a5
luca-gif <luca.89m@gmail.com>
2022-05-12 17:01:20 +0200

    Esercizio finito + bonus

---
+++ src/components/JumboComp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+    <img src="../assets/logo.png" >
+    
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'JumboComp'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TitleComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>
+    La mia prima app con Vue CLI!
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'TitleComp'
+}
+</script>
+
+<style>
+
+</style>
